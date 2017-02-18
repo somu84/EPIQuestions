d77c2e802db232c549a38efc988bef0da794c1fe
somu84 <someshwarchatterjee@gmail.com>
2017-02-18 10:56:39 -0500

    variance

--- src/com/epi/Arrays/Q6_BuySellSotcksOnce.java
+++ src/com/epi/Arrays/Q6_BuySellSotcksOnce.java
@@ -24,5 +24,28 @@ public class Q6_BuySellSotcksOnce {
 	public static void main(String args[]){
 		double pr[] = {310,315,275,295,260,270,290,230,255,250};
 		System.out.println("Max profile "+maxProfit(pr));
+		
+		int arr[] = {1,2,2,5,5,5,5,6,6,6,7,7,8};
+		System.out.println("Max Lenght: "+variance(arr));
+	}
+	
+	public static int variance(int[] arr){
+		int maxL = 1;
+		int currL = 1;
+		if(arr.length == 0)
+			return 0;
+		for(int i=1; i<arr.length; i++){
+			
+			if(arr[i] == arr[i-1]){
+				currL +=1;
+			}
+			else{
+				currL = 1;
+			}
+			if(maxL < currL){
+				maxL = currL;
+			}
+		}
+		return maxL;
 	}
 }
