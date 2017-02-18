574d0fe551d72c81e76d64a8dcc78e843810308d
somu84 <someshwarchatterjee@gmail.com>
2017-02-18 10:49:52 -0500

    q6

---
+++ src/com/epi/Arrays/Q6_BuySellSotcksOnce.java
@@ -0,0 +1,24 @@
+package com.epi.Arrays;
+
+public class Q6_BuySellSotcksOnce {
+
+	public static double maxProfit(double arr[]){
+		double max = 0;
+		double minpr = arr[0];
+		for(int i =1; i<arr.length; i++){
+			double curr = arr[i] - minpr;
+			if(curr > max){
+				max = curr;
+			}
+			if(minpr > arr[i]){
+				minpr = arr[i];
+			}
+		}
+		return max;
+	}
+	
+	public static void main(String args[]){
+		double pr[] = {310,315,275,295,260,270,290,230,255,250};
+		System.out.println("Max profile "+maxProfit(pr));
+	}
+}
