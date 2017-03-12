31db1830adc18273b6736c2f8c0e356923dfc53e
somu84 <someshwarchatterjee@gmail.com>
2017-03-12 14:31:11 -0400

    q7

---
+++ src/com/epi/PrimitiveDataTypes/Q7_Exponentiation.java
@@ -0,0 +1,21 @@
+package com.epi.PrimitiveDataTypes;
+
+public class Q7_Exponentiation {
+	public static double pow(double x, int y){
+		long pow = y;
+		double res = 1.0;
+		if(y < 0){
+			pow *= -1;
+			x = 1.0/x;
+		}
+		while(pow != 0){
+			if((pow&1)!=0){
+				res *= x;
+			}
+			x *= x;
+			pow >>=1;
+		}
+		return res;
+	}
+	
+}
