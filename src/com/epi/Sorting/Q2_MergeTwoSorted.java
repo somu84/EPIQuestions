0b9c892425d09b9a6aa255894f4633280e7fb589
somu84 <someshwarchatterjee@gmail.com>
2017-03-12 22:25:28 -0400

    q2

--- src/com/epi/Sorting/Q2_MergeTwoSorted.java
+++ src/com/epi/Sorting/Q2_MergeTwoSorted.java
@@ -1,5 +1,26 @@
 package com.epi.Sorting;
 
 public class Q2_MergeTwoSorted {
+	public static void merge(int[] a1, int[] a2, int m, int n){
+		int w = m+n-1;
+		int i=m, j=n;
+		while(i>=0 && j>= 0){
+			if(a1[i] > a2[j]){
+				a1[w] = a1[i];
+				i--;
+				w--;
+			}
+			else{
+				a1[w] = a2[j];
+				j--;
+				w--;
+			}
+		}
+		while(j>=0){
+			a1[w] = a2[j];
+			w--;
+			j--;
+		}
+	}
 
 }
