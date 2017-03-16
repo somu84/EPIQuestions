d16f37a1a3d6dc77ace3c56717896999cd21007c
somu84 <someshwarchatterjee@gmail.com>
2017-03-16 14:25:11 -0400

    Graph Rivision

--- src/com/epi/BinaryTrees/Q4_BinaryTree.java
+++ src/com/epi/BinaryTrees/Q4_BinaryTree.java
@@ -1,5 +1,41 @@
 package com.epi.BinaryTrees;
 
 public class Q4_BinaryTree {
+	static class Tree{
+		Tree parent;
+		Tree left;
+		Tree right;
+		int data;
+	}
 
+	public static Tree getLCA(Tree n1, Tree n2){
+		int d1 = getDepth(n1);
+		int d2 = getDepth(n2);
+		if(d1 < d2){
+			Tree temp = n1;
+			n1 = n2;
+			n2 = temp;
+		}
+		traverse(n1, Math.abs(d1-d2));
+		while(n1 != n2){
+			n1 = n1.parent;
+			n2 = n2.parent;
+		}
+		return n1;
+	}
+	
+	public static int getDepth(Tree node){
+		int depth = 0;
+		while(node != null){
+			depth++;
+			node = node.parent;
+		}
+		return depth;
+	}
+	
+	public static void traverse(Tree node, int diff){
+		while(diff-- > 0){
+			node = node.parent;
+		}
+	}
 }
---
+++ src/com/epi/Graphs/Q7_StringWithDistance.java
@@ -0,0 +1,92 @@
+package com.epi.Graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class Q7_StringWithDistance {
+	static class StringWithD{
+		String candidate;
+		int distance;
+		
+		StringWithD(String cS, int d){
+			this.candidate = cS;
+			this.distance = d;
+		}
+	}
+	
+	public static int transForm(Set<String> dict, String s, String e){
+		Queue<StringWithD> q = new LinkedList<>();
+		dict.remove(s);
+		q.add(new StringWithD(s, 0));
+		StringWithD f;
+		while(!q.isEmpty()){
+			f= q.poll();
+			if(f.candidate.equals(e)){
+				return f.distance;
+			}
+			String str = f.candidate;
+			for(int i=0; i<str.length(); i++){
+				String start = i==0?"":str.substring(0, i);
+				String end = i+1 < str.length()?str.substring(i+1):"";
+				for(int j=0; j<26;j++){
+					String modStr = start+(char)('a'+j)+end;
+					if(dict.remove(modStr)){
+						q.add(new StringWithD(modStr, f.distance+1));
+					}
+				}
+			}
+		}
+		return -1;
+	}
+	
+	//Get the actual strings
+	public static List<String> transForm1(Set<String> dict, String s, String e){
+		Queue<String> q = new LinkedList<>();
+		Map<String,String>prev = new HashMap<>();
+		//List<String>visited = new LinkedList<>();
+		q.add(s);
+		while(!q.isEmpty()){
+			String str = q.poll();
+			if(!dict.contains(str)){
+				continue;
+			}
+			dict.remove(str);
+			if(str.equals(e)){
+				List<String> res = new ArrayList<>();
+				res.add(e);
+				while(str!= null){
+					res.add(0,str);
+					str = prev.get(str);
+				}
+				return res;
+			}
+			List<String> nextWords = getOneWordEdit(str);
+			for(String next: nextWords){
+				if(dict.contains(next)){
+					prev.put(next, str);
+					q.add(next);
+				}
+			}
+		}
+		return null;
+	}
+	
+	public static List<String> getOneWordEdit(String str){
+		List<String> res = new ArrayList<>();
+		for(int i = 0 ; i<str.length(); i++){
+			String st = i==0?"":str.substring(0, i);
+			String end = i+1<str.length()?str.substring(i+1):"";
+			for(int j=0; j<=26;j++){
+				String modStr = st+(char)('a'+j)+end;
+				res.add(modStr);
+			}
+		}
+		return res;
+	}
+
+}
