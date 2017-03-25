555d526ea159a84bd311f10128cd28ba8a81f1d0
somu84 <someshwarchatterjee@gmail.com>
2017-03-25 13:10:31 -0400

    tree

---
+++ src/com/epi/BinaryTrees/Q12_IterativeInOrder.java
@@ -0,0 +1,28 @@
+package com.epi.BinaryTrees;
+import java.util.*;
+public class Q12_IterativeInOrder {
+	static class Tree{
+		Tree left;
+		Tree right;
+		int data;
+	}
+	
+	public static void inorder(Tree root){
+		Deque <Tree> stack = new LinkedList<>();
+		while(true){
+			if(root != null){
+				stack.offer(root);
+				root = root.left;
+			}
+			else{
+				if(stack.isEmpty()) break;
+				root = stack.pop();
+				System.out.println(root.data);
+				root = root.right;
+			}
+			
+		}
+		
+	}
+
+}
