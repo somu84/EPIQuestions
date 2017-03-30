eade130a62e43c73e2615dd83cdc1c49a61c6892
somu84 <someshwarchatterjee@gmail.com>
2017-03-29 22:48:47 -0400

    Q33 Rectangle

--- src/com/epi/Honors/Q33_MaxArray.java
+++ src/com/epi/Honors/Q33_MaxArray.java
@@ -1,5 +1,7 @@
 package com.epi.Honors;
 
+import java.util.*;
+
 public class Q33_MaxArray {
 	/**
 	 * https://www.youtube.com/watch?v=_Lf1looyJMU&t=21s
@@ -7,7 +9,7 @@ public class Q33_MaxArray {
 	 * @return
 	 */
 	public static int maxSubSquare(int[][]arr){
-		int max = Integer.MIN_VALUE;
+		int max = 0;
 		int[][]T = new int[arr.length+1][arr[0].length+1];
 		for(int i=1; i<T.length; i++){
 			for(int j=1; j<T[i].length; j++){
@@ -27,6 +29,71 @@ public class Q33_MaxArray {
 	
 	private static int min(int a, int b, int c){
 		return Math.min(a, Math.min(b, c));
+	}
+	
+	/**
+	 * https://www.youtube.com/watch?v=g8bSdXCG-lA&index=17&list=PLrmLmBdmIlpsHaNTPP_jHHDx_os9ItYXr
+	 * @param arr
+	 * @return
+	 */
+	public static int maxSubRectangle(int[][]arr){
+		int max = 0;
+		int[]T = new int[arr[0].length];
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				if(arr[i][j] == 0){
+					T[j] = 0;
+				}
+				else{
+					T[j]+=1;
+				}
+			}
+			int currMax = getMaxHistogram(T);
+			if(max < currMax){
+				max = currMax;
+			}
+		}
+		return max;
+	}
+	
+	/**
+	 * https://www.youtube.com/watch?v=ZmnqCZp9bBs&t=375s
+	 * @param arr
+	 * @return
+	 */
+	private static int getMaxHistogram(int[] arr){
+		Deque<Integer> st = new LinkedList<>();
+		int area = 0;
+		int maxArea = 0;
+		int i = 0;
+		for(i=0; i<arr.length; i++){
+			if(arr[i] >= st.peekFirst()|| st.isEmpty()){
+				st.push(i++);
+			}
+			else{
+				int top = st.pop();
+				if(st.isEmpty()){
+					area = arr[top]* i;
+				}else{
+					area = arr[top]*(i-st.peekFirst()-1);
+				}
+				if(area > maxArea){
+					maxArea = area;
+				}
+			}
+		}
+		while(!st.isEmpty()){
+			int j = st.pop();
+			if(st.isEmpty()){
+				area = arr[j]*i;
+			}else{
+				area = arr[j]*(i-st.peekFirst() - 1);
+			}
+			if(area > maxArea){
+				maxArea = area;
+			}
+		}
+		return maxArea;
 	}
 	
 	public static void main(String[] args){
