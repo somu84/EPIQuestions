af9bb0c5ed7cc010f0243ed25bef88d53907a389
somu84 <someshwarchatterjee@gmail.com>
2017-05-19 20:34:25 -0400

    practise

--- src/com/epi/Recursion/Extr_NumberOfValidCombinations.java
+++ src/com/epi/Recursion/Extr_NumberOfValidCombinations.java
@@ -15,14 +15,32 @@ public class Extr_NumberOfValidCombinations {
 				return getCnt(arr, ind-1);
 			}
 		}
-		if(arr[ind] > 0 && arr[ind]*10 + arr[ind+1] <= 26)
+		if(arr[ind] == 0)
+		if( arr[ind]*10 + arr[ind+1] <= 26 && arr[ind]*10 + arr[ind+1] >= 10)
 			return 1+getCnt(arr, ind-1);
 		return getCnt(arr, ind-1);
 	}
 	
+	private static int getCnt(int[] input) {
+	    int[] A = new int[input.length + 1];
+	    A[0] = 1;
+
+	    for(int i=1; i<input.length+1; i++) {
+	      A[i] = 0;
+	      if(input[i-1] > 0) {
+	        A[i] += A[i-1];
+	      }
+	      if (i > 1 && (10*input[i-2] + input[i-1]) <= 26) {
+	        A[i] += A[i-2];
+	      }
+	    }
+	    return A[input.length];
+	}
+	
 	public static void main(String[] args){
-		int[] arr = {4, 3, 0};
+		int[] arr = {1, 3, 0};
 		System.out.println(count(arr));
+		System.out.println(getCnt(arr));
 	}
 
 }
