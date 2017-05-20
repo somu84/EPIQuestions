38e663c4e3f2d64626b4fb33e11c302385cfc735
somu84 <someshwarchatterjee@gmail.com>
2017-05-20 10:09:25 -0400

    coins change problem

--- src/com/epi/DynamicProg/Q1_CountNumberOfWays.java
+++ src/com/epi/DynamicProg/Q1_CountNumberOfWays.java
@@ -25,6 +25,27 @@ public class Q1_CountNumberOfWays {
 		scores.add(3);
 		scores.add(7);
 		System.out.println(countWays(12, scores));
+		
+		int[]arr = {2, 3, 5};
+		System.out.println(getWays(arr, 12));
 	}
 
+	public static int getWays(int[]coins, int num){
+		if(coins == null || coins.length == 0 || num <= 0) 
+			return 0;
+		int[][]dp = new int[coins.length+1][num+1];
+		for(int i = 0; i<dp.length; i++){
+			dp[i][0] = 1;
+		}
+		for(int i = 1; i < dp.length ; i++){
+			for(int j = 1; j < dp[i].length; j++){
+				if(j < coins[i-1]){
+					dp[i][j] = dp[i-1][j];
+					continue;
+				}
+				dp[i][j] = dp[i-1][j]+dp[i][j - coins[i-1]];
+			}
+		}
+		return dp[coins.length][num];
+	}
 }
